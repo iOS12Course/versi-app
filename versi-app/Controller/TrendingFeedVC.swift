b76294b240bf71abc144f918deed307e0229b72c
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-04-21 19:41:39 -0500

    Building TrendingRepoCell subclass

--- versi-app/Controller/TrendingFeedVC.swift
+++ versi-app/Controller/TrendingFeedVC.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class TrendingFeedVC: UIViewController {
+class TrendingFeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.reloadData()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "trendingRepoCell", for: indexPath) as? TrendingRepoCell else { return UITableViewCell() }
+        let repo = Repo(image: UIImage(named: "searchIconLarge")!, name: "SWIFT", description: "Apples Programming Language", numberOfForks: 357, language: "Swift", numberOfContributors: 562, repoUrl: "www.google.com")
+        cell.configureCell(repo: repo)
+        return cell
     }
 }
 
