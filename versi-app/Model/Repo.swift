c378195a6af2c8a5d347de8eac95e7087ecc4fe4
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-04-21 19:09:58 -0500

    Building repo model layer

---
+++ versi-app/Model/Repo.swift
@@ -0,0 +1,31 @@
+//
+//  Repo.swift
+//  versi-app
+//
+//  Created by Ricardo Herrera Petit on 4/21/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class Repo {
+    public private(set) var image: UIImage
+    public private(set) var name: String
+    public private(set) var description: String
+    public private(set) var numberOfForks : Int
+    public private(set) var language: String
+    public private(set) var numberOfContributors: Int
+    public private(set) var repoUrl: String
+    
+    init(image: UIImage, name:String, description:String, numberOfForks:Int, language:String, numberOfContributors:Int, repoUrl:String) {
+        self.image = image
+        self.name = name
+        self.description = description
+        self.numberOfForks = numberOfForks
+        self.language = language
+        self.numberOfContributors = numberOfContributors
+        self.repoUrl = repoUrl
+    }
+}
+
+
